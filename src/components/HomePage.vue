2708661f030a9a661af98c87fe655b70eeaebc0d
Frank Kulak <fs.kulak@gmail.com>
2020-08-15 14:51:43 -0400

    Work on home page redesign.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,44 +3,33 @@
         <b-row id="welcome" align-h="center">
             <b-col cols="12" md="10" lg="8">
                 <h1>welcome</h1>
-                <p>My name is Frank, and I make mods for The Sims 4. I share them on this website - you can browse
-                    through them below, and click their cards to learn more.</p>
+                <p>I'm Frank, and I make mods for The Sims 4 - you can browse and download them from below. Please
+                    report bugs and direct questions to <a href="mailto:mods@frankkulak.com">mods@frankkulak.com</a>.
+                </p>
             </b-col>
         </b-row>
 
-        <b-row id="mods" align-h="center" class="mods-display">
-            <b-col cols="12" sm="8" md="5" lg="4" v-for="(mod, index) in activeMods" :key="index">
-                <mod-preview :mod="mod"></mod-preview>
-            </b-col>
-        </b-row>
+        <mod-category-header header="active mods"
+                             description="These mods have been tested thoroughly and are ready to be downloaded. They
+                             are updated often, so be sure to check back after every game patch!"/>
+        <mod-category-content :mods="activeMods"/>
 
-        <b-row id="retired-header" align-h="center" v-if="retiredMods.length > 0">
-            <b-col cols="12" md="10" lg="8">
-                <h1>retired mods</h1>
-                <p>The below mods are no longer being updated, and may not work with the latest patch.</p>
-            </b-col>
-        </b-row>
+        <mod-category-header header="work-in-progress mods"
+                             description="These mods are currently being worked on, and are not yet available for
+                             download. Check back later if you are interested in any of them!"/>
+        <mod-category-content :mods="wipMods"/>
 
-        <b-row id="retired-mods" align-h="center" v-if="retiredMods.length > 0" class="mods-display">
-            <b-col cols="12" sm="8" md="5" lg="4" v-for="(mod, index) in retiredMods" :key="index">
-                <mod-preview :mod="mod"></mod-preview>
-            </b-col>
-        </b-row>
-
-        <b-row id="contact" align-h="center">
-            <b-col cols="12" md="10" lg="8">
-                <h1>contact</h1>
-                <p>email: <a href="mailto:mods@frankkulak.com">mods@frankkulak.com</a></p>
-                <p>Feel free to report issues, ask questions, or provide feedback about my current mods, but please
-                    refrain from sending mod requests.</p>
-            </b-col>
-        </b-row>
+        <mod-category-header header="retired mods"
+                             description="These mods are still available for download, but are no longer being updated
+                             and may conflict with the latest patch."/>
+        <mod-category-content :mods="retiredMods"/>
     </b-container>
 </template>
 
 <script>
     import {ModData} from '../modules/Data.js'
-    import ModPreview from "./ModPreview";
+    import ModCategoryContent from "./ModCategoryContent";
+    import ModCategoryHeader from "./ModCategoryHeader";
 
     const mods = Object.values(ModData.ts4).sort((mod1, mod2) => {
         const modName1 = mod1.name.toUpperCase();
@@ -56,11 +45,12 @@
 
     export default {
         name: "HomePage",
-        components: {ModPreview},
+        components: {ModCategoryHeader, ModCategoryContent},
         data: function () {
             return {
                 activeMods: mods.filter(mod => !mod.retired),
-                retiredMods: mods.filter(mod => mod.retired)
+                retiredMods: mods.filter(mod => mod.retired),
+                wipMods: []
             }
         }
     }
@@ -77,29 +67,23 @@
         }
 
         #welcome {
-            background-color: var(--bg-band-overlay);
+            background-color: $dark-blue;
             padding-top: $padding-lg;
             padding-bottom: $padding-lg;
-        }
 
-        #mods {
-            [class^="col"] {
-                margin: {
-                    top: $padding-sm;
-                    bottom: $padding-sm;
-                }
+            img.decal {
+                max-width: 300px;
+            }
+
+            p {
+                color: var(--light-color);
             }
         }
+
 
         .mods-display {
             padding-top: $padding-lg - $padding-sm;
             padding-bottom: $padding-lg - $padding-sm;
-        }
-
-        #contact {
-            background-color: var(--bg-band-overlay);
-            padding-top: $padding-lg;
-            padding-bottom: $padding-lg;
         }
     }
 </style>
---
+++ src/components/ModCategoryHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-row align-h="center" class="mod-category-header">
+        <b-col cols="12" md="8" lg="6" class="text-center">
+            <h1>{{ header }}</h1>
+            <p>{{ description }}</p>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+    export default {
+        name: "ModCategoryHeader",
+        props: {
+            description: String,
+            header: String
+        }
+    }
+</script>
+
+<style lang="scss">
+    .mod-category-header {
+        padding: {
+            top: $padding-lg;
+            bottom: $padding-lg;
+        };
+    }
+</style>
