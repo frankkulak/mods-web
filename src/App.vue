f613b4f293564dc466c489d324e6ed141af72a7e
Frank Kulak <fs.kulak@gmail.com>
2020-11-14 22:46:46 -0500

    Add Patreon footer.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="app">
         <router-view></router-view>
+
+        <section id="patreon-footer" class="dark-vars py-4 text-center">
+            <b-container fluid>
+                <p>If you enjoy my work, please consider
+                    <a href="https://www.patreon.com/frankkulakmods" target="_blank">supporting me on Patreon</a>.
+                </p>
+            </b-container>
+        </section>
     </div>
 </template>
 
@@ -11,4 +19,7 @@
 </script>
 
 <style lang="scss">
+    #patreon-footer {
+        background-color: $darker-blue;
+    }
 </style>
