aeeeb045bd576e5da045eada76d8979ec8a663ad
Frank Kulak <fs.kulak@gmail.com>
2021-09-05 15:03:25 -0400

    Add keyboard shortcut to make a new string.

--- src/components/Tools/StblEditor.vue
+++ src/components/Tools/StblEditor.vue
@@ -2,54 +2,56 @@
     <b-container id="stbl-editor-container" class="p-5" fluid>
         <div class="mb-5">
             <div class="mb-5">
-                <h1 class="mb-3">Easy String Table Editor</h1>
-                <p>This experimental tool provides a graphical interface for creating and editing string tables. It
-                    currently only works with one string table binary (<code>.binary</code> or <code>.stbl</code> files)
-                    at a time, but support for packages and multiple string tables will come soon™. Reach out to me on
-                    Discord if you experience any issues while using it.</p>
-                <p>Please note that this tool does not edit the file you upload, but rather uses it as a starting point,
-                    and then lets you download a modified version.</p>
+                <h1 class="mb-3">Easy String Tables</h1>
+                <p>This experimental tool provides an interface for creating and editing string tables. It
+                    currently only works with string table binaries (<code>.binary</code>/<code>.stbl</code> files),
+                    and can only edit one at a time. Support for packages and multiple string tables will come soon™.
+                    Reach out to me on Discord if you experience any issues while using it.</p>
+                <p>Please note that this tool does not edit the files you upload. It just uses their contents as a
+                    starting point, and then lets you download a modified version.</p>
             </div>
 
             <div class="mb-5">
                 <section-header text="how to use" class="mb-4"></section-header>
-                <p><strong>Step 1:</strong> Upload an existing <code>.binary</code> or <code>.stbl</code> file with the
-                    input below, or create a new, blank string table with the "New String Table" button.</p>
-                <p><strong>Step 2:</strong> Add to, edit, and delete from your string table.</p>
+                <p><strong>Step 1:</strong> Upload an existing <code>.binary</code>/<code>.stbl</code> file, or
+                    create a fresh one with the "New String Table" button. This will bring up the editor.</p>
+                <p><strong>Step 2:</strong> Modify and interact with your string table.</p>
                 <ul>
-                    <li class="mb-2"><strong>Adding a string.</strong> Use the
+                    <li class="mb-2"><strong>Adding.</strong> Either use the
                         <b-icon-plus-circle/>
-                        button near the bottom of the string list to create a new string entry. When done typing out
-                        your new string, you can either generate a key for it by hovering over
+                        button in the bottom-right corner of the screen or press <kbd>CTRL/CMD</kbd> + <kbd>N</kbd> to add a string. A popup will prompt you for a
+                        string to add, and will automatically hash and add it to the table when you are done.
+                    </li>
+                    <li class="mb-2"><strong>Editing.</strong> Simply click on the string you would like to edit, and
+                        start typing. Editing a string will <strong>not</strong> rehash its key, but if you would like
+                        to generate a new hash, you can hover over
                         <b-icon-three-dots/>
-                        and clicking the
+                        and click the
                         <b-icon-arrow-repeat/>
-                        icon, or type it in yourself with the
+                        icon. To manually set the key, click the
                         <b-icon-key/>
                         icon.
                     </li>
-                    <li class="mb-2"><strong>Editing a string.</strong> Simply click on a string's text area and type.
-                        Your changes will be saved automatically.
-                    </li>
-                    <li class="mb-2"><strong>Deleting a string.</strong> Hover over
+                    <li class="mb-2"><strong>Deleting.</strong> Hover over
                         <b-icon-three-dots/>
                         and click the
                         <b-icon-trash-fill variant="danger"/>
-                        button. You will be asked to confirm if you want to delete the string, because this action
-                        cannot be undone.
+                        button. You will then be asked for confirmation, because this action cannot be undone.
                     </li>
-                    <li class="mb-2"><strong>Copying strings.</strong> To copy the key (<code>0x00000000</code>),
+                    <li class="mb-2"><strong>Copying.</strong> To copy the key (<code>0x00000000</code>),
                         click the
                         <b-icon-clipboard/>
                         button. To copy both the key and string (<code>0x00000000&lt;!--String--&gt;</code>), use
                         <b-icon-clipboard-plus/>
                         instead.
                     </li>
-                    <li class="mb-2"><strong>Localization.</strong> You can choose from any of the languages
-                        that The Sims 4 supports, and the file name will be formatted correctly when you export it.
+                    <li class="mb-2"><strong>Localizing.</strong> Choose the language that your string table is written
+                        in, and the program will use the correct hash prefix when exporting your file.
                     </li>
                 </ul>
-                <p><strong>Step 3:</strong> Download the modified string table.</p>
+                <p><strong>Step 3:</strong> Download your modified string table with the
+                    <b-icon-download/>
+                    button in the bottom-right corner of the screen.</p>
             </div>
         </div>
 
@@ -66,8 +68,7 @@
                 ></b-form-file>
             </b-col>
             <b-col cols="12" md="6" class="text-right">
-                <b-button id="new-stbl-btn" v-on:click="printStbl()" pill>
-                    <b-icon-plus></b-icon-plus>
+                <b-button id="new-stbl-btn" v-on:click="newStbl()" class="gradient-button" pill>
                     New String Table
                 </b-button>
             </b-col>
@@ -80,8 +81,24 @@
             <pre class="mb-0">{{ errorMessage }}</pre>
         </b-alert>
 
-        <b-row v-if="fileContents" id="stbl-content">
-            <b-col cols="6" class="my-3">
+        <b-row v-if="Array.isArray(fileContents)" id="stbl-content">
+            <div id="utility-buttons-container">
+                <b-button-toolbar class="text-right float-right floating-card">
+                    <b-button-group>
+                        <b-button v-on:click="downloadStbl()">
+                            <b-icon-download/>
+                        </b-button>
+                        <b-button v-on:click="newString()">
+                            <b-icon-plus-circle/>
+                        </b-button>
+                    </b-button-group>
+                </b-button-toolbar>
+            </div>
+
+            <b-col cols="12">
+                <hr id="content-divider" class="mb-5">
+            </b-col>
+            <b-col cols="12" md="6" class="my-3">
                 <b-form-select name="language" v-model="languageCode">
                     <option
                         v-for="language in languages"
@@ -91,7 +108,13 @@
                     </option>
                 </b-form-select>
             </b-col>
-            <b-col cols="6"></b-col>
+            <b-col cols="12" md="6"></b-col>
+            <b-col cols="12" v-if="fileContents === null || fileContents.length === 0" class="my-5 text-center">
+                <p>This string table is empty. To add a new string, either click the
+                    <b-icon-plus-circle/>
+                    button, press the <kbd>CTRL/CMD</kbd> + <kbd>N</kbd> keys, or
+                    <span v-on:click="newString" class="clickable">click here</span>.</p>
+            </b-col>
             <b-col cols="12" md="6" v-for="(stringEntry, n) in fileContents" :key="n" class="my-3">
                 <div>
                     <b-card class="floating-card">
@@ -149,9 +172,6 @@
                     </b-card>
                 </div>
             </b-col>
-            <b-col cols="12" md="6" class="my-3 text-center h-100" style="font-size: 3rem;">
-                <b-icon-plus-circle class="blurple-text hover-cursor my-auto" v-on:click="newString()"/>
-            </b-col>
         </b-row>
     </b-container>
 </template>
@@ -159,14 +179,14 @@
 <script>
 import SectionHeader from "@/components/Common/SectionHeader";
 import {
-    BIconPlus,
     BIconPlusCircle,
     BIconTrashFill,
     BIconClipboard,
     BIconClipboardPlus,
     BIconArrowRepeat,
     BIconThreeDots,
-    BIconKey
+    BIconKey,
+    BIconDownload
 } from 'bootstrap-vue';
 import {getStblContents, Languages, fnv32a} from "@/components/scripts/stblReader";
 
@@ -176,18 +196,33 @@ function formatKeyAsHex(keyDec) {
 }
 
 
+const previousHandlers = [];
+
+
 export default {
     name: "StblEditor",
     components: {
         SectionHeader,
         BIconPlusCircle,
-        BIconPlus,
         BIconTrashFill,
         BIconClipboard,
         BIconArrowRepeat,
         BIconThreeDots,
         BIconClipboardPlus,
-        BIconKey
+        BIconKey,
+        BIconDownload
+    },
+    created() {
+        if (previousHandlers.length > 0) {
+            previousHandlers.forEach(handler => {
+                document.removeEventListener('keydown', handler);
+            });
+
+            previousHandlers.length = 0;
+        }
+
+        document.addEventListener('keydown', this.handleKeydown);
+        previousHandlers.push(this.handleKeydown);
     },
     data() {
         return {
@@ -195,10 +230,25 @@ export default {
             fileContents: null,
             errorMessage: null,
             languages: Languages,
-            languageCode: '00'
+            languageCode: '00',
+            isAskingForString: false
         }
     },
     methods: {
+        handleKeydown(event) {
+            const keyComboPassed = (event.ctrlKey || event.metaKey) && event.key === 'n';
+            if (keyComboPassed && (Array.isArray(this.fileContents))) {
+                this.newString();
+            }
+        },
+        newStbl() {
+            if (this.fileContents === null) {
+                this.fileContents = [];
+            } else if (this.fileContents.length > 0) {
+                if (confirm("Are you sure you want to overwrite the string table you currently have open? Its contents cannot be recovered once you do this."))
+                    this.fileContents = [];
+            }
+        },
         getHexCode(index) {
             return formatKeyAsHex(this.fileContents[index].key);
         },
@@ -210,15 +260,10 @@ export default {
                 } else {
                     this.errorMessage = null;
                     this.fileContents = result.stringEntries.map(entry => {
-                        return { key: entry.key, string: entry.string };
+                        return {key: entry.key, string: entry.string};
                     });
                 }
-            }).then(() => {
-                document.getElementById('upload-area').scrollIntoView();
             });
-        },
-        printStbl() {
-            console.log(this.fileContents);
         },
         deleteString(index) {
             if (confirm(`Are you sure you want to delete string ${formatKeyAsHex(this.fileContents[index].key)}? This action cannot be undone.`)) {
@@ -226,7 +271,11 @@ export default {
             }
         },
         newString() {
-            this.fileContents.push({key: 0, string: ""});
+            this.isAskingForString = true;
+            const string = prompt("Enter a string.");
+            const key = fnv32a(string);
+            this.fileContents.push({key, string});
+            this.isAskingForString = false;
         },
         newHash(index) {
             const stringEntry = this.fileContents[index];
@@ -236,7 +285,7 @@ export default {
             return formatKeyAsHex(this.fileContents[index].key);
         },
         setManualKey(index) {
-            let newKey = prompt("Enter the 32-bit hash to use.", this.getHexCode(index));
+            const newKey = prompt("Enter the 32-bit hash to use.", this.getHexCode(index));
             if (newKey && newKey.match(/^0x([0-9A-F]{8})$/i)) {
                 this.fileContents[index].key = parseInt(newKey, 16);
             } else {
@@ -246,6 +295,9 @@ export default {
         keyAndStringToClipboard(index) {
             const stringEntry = this.fileContents[index];
             return formatKeyAsHex(stringEntry.key) + "<!--" + stringEntry.string + "-->";
+        },
+        downloadStbl() {
+            console.log(this.fileContents);
         }
     }
 }
@@ -263,7 +315,7 @@ export default {
         box-shadow: 0 0 0 0.2rem transparentize($blurple, 0.8);
     }
 
-    button#new-stbl-btn {
+    button.gradient-button {
         @extend %default-gradient;
         @extend %floating-card;
 
@@ -281,6 +333,21 @@ export default {
         }
     }
 
+    #utility-buttons-container {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        z-index: 1024;
+    }
+
+    hr#content-divider {
+        border-style: solid;
+        border-radius: 2px;
+        border-width: 1px;
+        border-color: var(--accent-color);
+        width: 100%;
+    }
+
     .floating-card {
         @extend %floating-card;
     }
@@ -291,6 +358,25 @@ export default {
 
     .blurple-text {
         color: $blurple;
+    }
+
+    .clickable {
+        color: $blurple;
+        text-decoration: underline;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    kbd {
+        background-color: white;
+        color: $dark-gray;
+        border-color: $dark-gray;
+        border-radius: 6px;
+        border-width: 1px;
+        font-size: 0.8em;
+        border-style: solid;
     }
 }
 </style>
