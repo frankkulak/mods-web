3bde9d6a6baea18ddb480c269c4680c88998db1b
Frank Kulak <fs.kulak@gmail.com>
2021-09-26 12:35:59 -0400

    Add dark mode toggle.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,10 @@
                     <b-nav-item to="/tutorials">tutorials</b-nav-item>
                     <b-nav-item to="/about">info &amp; contact</b-nav-item>
                 </b-navbar-nav>
+                <div id="theme-switcher" class="ml-md-2" @click="changeTheme">
+                    <b-icon-moon v-if="isDarkTheme"/>
+                    <b-icon-sun v-else/>
+                </div>
             </b-collapse>
         </b-navbar>
 
@@ -24,9 +28,26 @@
 
 <script>
     import MyFooter from "@/components/Common/MyFooter";
+    import { BIconMoon, BIconSun } from "bootstrap-vue";
     export default {
         name: 'App',
-        components: {MyFooter}
+        components: {MyFooter, BIconSun, BIconMoon},
+        data() {
+            return {
+                isDarkTheme: undefined
+            }
+        },
+        created() {
+            this.isDarkTheme = localStorage.getItem('fkmods_IsDarkTheme') === "true";
+            document.documentElement.setAttribute('data-theme', this.isDarkTheme ? 'dark' : 'light');
+        },
+        methods: {
+            changeTheme() {
+                this.isDarkTheme = !this.isDarkTheme;
+                document.documentElement.setAttribute('data-theme', this.isDarkTheme ? 'dark' : 'light');
+                localStorage.setItem('fkmods_IsDarkTheme', this.isDarkTheme);
+            }
+        }
     }
 </script>
 
@@ -57,6 +78,16 @@
             a.router-link-exact-active {
                 color: var(--accent-color) !important;
                 font-weight: bold;
+            }
+        }
+
+        #theme-switcher {
+            font-size: 1.5em;
+            opacity: 60%;
+
+            &:hover {
+                cursor: pointer;
+                opacity: 100%;
             }
         }
     }
