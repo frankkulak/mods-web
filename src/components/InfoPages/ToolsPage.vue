35c3a393037904f2a5fd3c4adb9b09aff8fde6d6
Frank Kulak <fs.kulak@gmail.com>
2022-09-12 09:44:00 -0400

    Update ToolsPage.vue

--- src/components/InfoPages/ToolsPage.vue
+++ src/components/InfoPages/ToolsPage.vue
@@ -1,33 +1,34 @@
 <template>
     <b-container id="tools-page" class="py-5" fluid>
         <b-container fluid>
-            <section-header text="web tools" class="mb-4"/>
-            <p>Please note that these tools are currently considered experimental, and you should double-check
-                their output before using anything they produce in your releases.</p>
+            <section-header text="Sims 4 Toolkit" class="mb-4"/>
+            <p>Sims 4 Toolkit (S4TK) is a family of modding tools created by me. The core library powers all of the
+                other tools in this category.</p>
             <b-row>
                 <b-col cols="12" sm="6" class="tool-col p-4">
-                    <a class="tutorial-link" href="#/ts4-tools/stbl-editor">
-                        <b-row align-h="center" class="tutorial-link-row text-left mb-3 py-3 h-100">
+                    <a class="tutorial-link" href="https://sims4toolkit.com" target="_blank">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
                             <b-col cols="12">
-                                <h3 class="font-weight-bold">String Table Editor</h3>
-                                <p class="mt-3">Allows you to create, modify, and search through string tables of any
-                                    size (tested up to 60k!). Features include autohashing, autosave, quick copying, two
-                                    views to choose from, and more. Be sure to check out the settings (click on the gear
-                                    icon after opening a string table) to customize your experience.</p>
+                                <h3 class="font-weight-bold">Core Library</h3>
+                                <p class="mt-3">
+                                    A Node.js library for reading and writing .package files and the resources they
+                                    contain. This library is intended for use with web tools and build scripts. All
+                                    aspects of Sims 4 Toolkit are in active development, and are subject to change.
+                                </p>
                             </b-col>
                         </b-row>
                     </a>
                 </b-col>
 
                 <b-col cols="12" sm="6" class="tool-col p-4">
-                    <a class="tutorial-link" href="#/ts4-tools/stbl-merger">
-                        <b-row align-h="center" class="tutorial-link-row text-left mb-3 py-3 h-100">
+                    <a class="tutorial-link" href="https://stbl.sims4toolkit.com" target="_blank">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
                             <b-col cols="12">
-                                <h3 class="font-weight-bold">String Table Merger</h3>
-                                <p class="mt-3">Allows you to quickly and easily combine many string tables into one.
-                                    You can choose the language, type, group, and instance of the file that is output,
-                                    as well as the naming convention to use. Support for merging multiple languages at a
-                                    time is planned, but not implemented yet.</p>
+                                <h3 class="font-weight-bold">String Table Studio</h3>
+                                <p class="mt-3">The new and improved string table editor. It has all the features of the
+                                    previous one, plus: unlimited projects/locales/strings, error detection, translation
+                                    mode, uploading/downloading packages, advanced search tools, token assistance, and
+                                    more.</p>
                             </b-col>
                         </b-row>
                     </a>
@@ -35,7 +36,7 @@
 
                 <b-col cols="12" sm="6" class="tool-col p-4">
                     <a class="tutorial-link" href="https://pkg.sims4toolkit.com/#/view" target="_blank">
-                        <b-row align-h="center" class="tutorial-link-row text-left mb-3 py-3 h-100">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
                             <b-col cols="12">
                                 <h3 class="font-weight-bold">Package Scanner</h3>
                                 <p class="mt-3">Upload your package to have a pair of robot eyes scan it for common
@@ -47,8 +48,21 @@
                 </b-col>
 
                 <b-col cols="12" sm="6" class="tool-col p-4">
-                    <a class="tutorial-link" href="https://stbl.sims4toolkit.com/#/batch-fix/pronouns" target="_blank">
-                        <b-row align-h="center" class="tutorial-link-row text-left mb-3 py-3 h-100">
+                    <a class="tutorial-link" href="https://pkg.sims4toolkit.com/#/generate" target="_blank">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
+                            <b-col cols="12">
+                                <h3 class="font-weight-bold">Tuning Generator</h3>
+                                <p class="mt-3">Generate tuning and SimData files in bulk, using your own templates.
+                                    Note that this is a temporary and experimental tool while file generation is being
+                                    added to the core library.</p>
+                            </b-col>
+                        </b-row>
+                    </a>
+                </b-col>
+
+                <b-col cols="12" sm="6" class="tool-col p-4">
+                    <a class="tutorial-link" href="https://stbl.sims4toolkit.com/#/tools/pronouns" target="_blank">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
                             <b-col cols="12">
                                 <h3 class="font-weight-bold">Pronoun Batch Fix Tool</h3>
                                 <p class="mt-3">The pronoun update changed how some strings work, and this online tool
@@ -60,19 +74,31 @@
             </b-row>
         </b-container>
         <b-container class="mt-5">
-            <section-header text="other tools" class="mb-4"/>
+            <section-header text="old web tools" class="mb-4"/>
+            <p>Please note that these tools are now obsolete due to the new and improved String Table Studio. These
+                tools will be decommissioned in the future, so it's recommended that you migrate now.</p>
             <b-row>
                 <b-col cols="12" sm="6" class="tool-col p-4">
-                    <a class="tutorial-link" href="https://sims4toolkit.com" target="_blank">
-                        <b-row align-h="center" class="tutorial-link-row text-left mb-3 py-3 h-100">
+                    <a class="tutorial-link" href="#/ts4-tools/stbl-editor">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
                             <b-col cols="12">
-                                <h3 class="font-weight-bold">Sims 4 Toolkit</h3>
-                                <p class="mt-3">
-                                    A Node.js library for reading and writing .package files and the resources they
-                                    contain. All aspects of Sims 4 Toolkit are in active development, and are subject to
-                                    change. Use at your own risk, knowing that breaking changes are likely to occur
-                                    until version 1.0.0 is released.
-                                </p>
+                                <h3 class="font-weight-bold">String Table Editor</h3>
+                                <p class="mt-3">Allows you to create, modify, and search through string tables of any
+                                    size (tested up to 60k!). Features include autohashing, autosave, quick copying, two
+                                    views to choose from, and more.</p>
+                            </b-col>
+                        </b-row>
+                    </a>
+                </b-col>
+
+                <b-col cols="12" sm="6" class="tool-col p-4">
+                    <a class="tutorial-link" href="#/ts4-tools/stbl-merger">
+                        <b-row align-h="center" class="tutorial-link-row text-left py-3 h-100">
+                            <b-col cols="12">
+                                <h3 class="font-weight-bold">String Table Merger</h3>
+                                <p class="mt-3">Allows you to quickly and easily combine many string tables into one.
+                                    You can choose the language, type, group, and instance of the file that is output,
+                                    as well as the naming convention to use.</p>
                             </b-col>
                         </b-row>
                     </a>
